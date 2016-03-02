8a483a106f97a2b1eaf1dc019580803a22c78c28
orels1 <orel-s1@yandex.ru>
2016-03-03 00:32:22 +0300

    updated db name

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
  * Created by orel- on 06/Dec/15.
  */
 module.exports = {
-    database: process.env.MONGOLAB_URI || 'localhost/moders2'
+    database: process.env.MONGOLAB_URI || 'localhost/red-cloudbank'
 };
