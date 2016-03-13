25b0294a37664675454775e0782b339e3d6fe62a
orels1 <orel-s1@yandex.ru>
2016-03-13 17:27:30 +0300

    Changed project homepage

--- app/components/Home.js
+++ app/components/Home.js
@@ -43,8 +43,29 @@ class Home extends React.Component {
             <div>
                 <div className="clearfix"></div>
 
-                <div className="col-md-12">
-                    <CogsList params={{limit:3}} />
+                <div className="col-md-10 col-md-offset-1">
+                    <h2 className="welcome"> Welcome to Red-Cloudbank! </h2>
+                    <div className="panel panel-default">
+                        <div className="panel-body">
+                            <p>This is the place for all the custom cogs to live for your amusement :D</p>
+                            <p>
+                                To view all the cogs, please use the <Link to="/list"><b>List</b></Link> link in a topbar.
+                                <br />
+                                There you will find cogs, their descriptions and installation instructions, so you won't need to type the commands yourself (it's 21 century, for god's sake! :D ).
+                            </p>
+                            <p>If you don't know what Red is, and what's it all about - check this <a href="https://github.com/Twentysix26/Red-DiscordBot"><b>repo</b></a>, you will find all the info there.</p>
+                            <p>
+                                That's all there is to it! Feel free to explore, add your own cogs (check the <Link to="/add"><b>Add</b></Link> link in a topbar to find out how) and have a good time.
+                             </p>
+                            <p>
+                                If you have any questions or suggestions - feel free to message me <a href="https://discord.gg/0k4npTwMvTo9TQoj"><b>here</b></a>, I go by "orels1" nickname.
+                                <br />
+                                If you encounter any bugs or other issues - please add it an issue tracker <a href="https://github.com/orels1/red-cloudbank/issues"><b>here</b></a>, I will be very thankful.
+                            </p>
+                            <p>All credits for Red goes to an awesome <a href="https://github.com/Twentysix26"><b>Twentysix26</b></a>, you can support him <a href="https://www.patreon.com/Twentysix26?ty=h"><b>here</b></a>.</p>
+
+                            </div>
+                    </div>
                 </div>
             </div>
         );
